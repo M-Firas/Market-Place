1e932aa9c1d99c3a514292a5ccb68b08b7777b1f
WessamDah <wessamdahrouj@gmail.com>
2025-04-06 23:14:48 +0300

    create the header

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Profile from "./pages/Profile";
 import About from "./pages/About";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
