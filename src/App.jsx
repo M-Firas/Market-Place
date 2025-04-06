2f7f16fd2ecc3c7346cba935f3cfea8a77e5eade
WessamDah <wessamdahrouj@gmail.com>
2025-04-06 22:22:29 +0300

    make the routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// components
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 function App() {
-  return <h1 class="text-3xl font-bold underline   ">Hello world!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
