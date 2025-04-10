33a640426c85d9048dd7dfae17201ac219109e64
WessamDah <wessamdahrouj@gmail.com>
2025-04-10 23:57:07 +0300

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,19 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import router from "./routes/user.route.js";
-dotenv.config();
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
+dotenv.config({ path: "./.env" });
+
+console.log("URI:", process.env.MONGO);
+console.log("ALL ENV VARS:", process.env);
 
 mongoose
-  .connect(process.env.MONGO)
+  .connect(process.env.MONGO, {
+    serverSelectionTimeoutMS: 20000, // <- Wait up to 20s before timeout
+  })
   .then(() => {
     console.log("connected to MongoDB!");
   })
   .catch((err) => {
-    console.log(err);
+    console.error("MongoDB connection error:", err);
   });
 
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("server is running on port 3000!");
@@ -25,4 +34,5 @@ app.get("/", (req, res) => {
   });
 });
 
-app.use("/api/user", router);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
