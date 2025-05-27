50833eb3aa144f7f19b230ae06c1b47fd83a0646
Firas M <firas@testing.com>
2025-05-27 23:34:50 +0300

    trying to fix cookie issue

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -42,16 +42,13 @@ export default function OAuth() {
       dispatch(signInSuccess(data));
       // Fetching the actual current user from the backend after signing in (based on the cookie)
       dispatch(currentUserStart());
-      const userRes = await fetch(
-        "https://market-place-jj5i.onrender.com/api/user/getCurrentUser",
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
+      const userRes = await fetch("/api/user/getCurrentUser", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
         },
-      );
+        credentials: "include",
+      });
 
       if (!userRes.ok) {
         dispatch(currentUserFailure("Failed to get user data"));
